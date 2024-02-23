2518ae125f04871bd9a02ed6be5afd1353388061
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-02-24 02:33:01 +0500

    basic arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
-use std::path::Path;
-use wall_rs::config::Config;
+use clap::Parser;
+use wall_rs::{Cli, Commands};
 
-static CONFIG: &str = "./config.toml";
+// use std::path::Path;
+// use wall_rs::config::Config;
+// static CONFIG: &str = "./config.toml";
+// fn main() {
+//     let conf = Config::new("/Users/sakhib/Developer/github/wall-rs".to_string(), 32);
+//
+//     conf.write(CONFIG.to_string()).unwrap();
+//
+//     let another_one = Config::from_file(Path::new(CONFIG));
+//
+//     println!("Read file: {:?}", another_one);
+// }
+
 
 fn main() {
-    let conf = Config::new("/Users/sakhib/Developer/github/wall-rs".to_string(), 32);
+    let args = Cli::parse();
 
-    conf.write(CONFIG.to_string()).unwrap();
-
-    let another_one = Config::from_file(Path::new(CONFIG));
-
-    println!("Read file: {:?}", another_one);
+    match args.command {
+        Commands::Set { remote } => {
+            println!("{}", remote)
+        }
+    };
 }
