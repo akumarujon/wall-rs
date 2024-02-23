33b0aed1b1f10505080533805ec34c567b5fbb2d
akumaru <isakulovdev@gmail.com>
2024-02-23 15:51:37 +0500

    cross-platform path & refactor

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,15 @@ use rand;
 
 use wallpaper;
 
+/// Checks if the specified folder is empty.
+///
+/// ## Arguments
+///
+/// * `folder_path` - A string slice that holds the path of the folder to check.
+///
+/// ## Returns
+///
+/// * `bool` - Returns true if the folder is empty, otherwise false.
 fn is_folder_empty(folder_path: &str) -> bool {
     if let Ok(entries) = fs::read_dir(folder_path) {
         return entries.count() == 0
@@ -14,15 +23,20 @@ fn is_folder_empty(folder_path: &str) -> bool {
 
 
 fn main() {
-    let _path: String = "/home/t34/dev/projects/wall-rs/pics/Kaori_Miyazono_Rust.png".to_string();
-    let wallpapers_path = "/home/t34/dev/projects/wall-rs/pics/";
+    let wallpapers_path = if cfg!(windows) {
+        format!("{}\\.wallpapers", std::env::var("HOMEPATH").unwrap())
+    } else {
+        format!("{}/.wallpapers", std::env::var("HOME").unwrap())
+    };
 
+
+    // TODO change this to own mirror repository
     let _url = "https://github.com/cat-milk/Anime-Girls-Holding-Programming-Books";
 
     
 
-    if !Path::new(&wallpapers_path).exists() {
-            match fs::create_dir(wallpapers_path) {
+    if !Path::new(&wallpapers_path.clone()).exists() {
+            match fs::create_dir(wallpapers_path.clone()) {
                 Ok(()) => {
                     println!("Folder created.");
                 },
@@ -31,8 +45,10 @@ fn main() {
 
     } 
     
-    if is_folder_empty(wallpapers_path) {
-        println!("Folder is empty");    
+
+    // TODO: download all the images from the mirror.
+    if is_folder_empty(wallpapers_path.clone().as_str()) {
+        println!("Folder is empty");   
     }
 
     let paths = match fs::read_dir(wallpapers_path) {
@@ -47,12 +63,6 @@ fn main() {
 
     let mut rng = rand::thread_rng(); 
     let path = paths.choose(&mut rng).unwrap();
-
-    println!("{}", path.display());
-
-    if !Path::new(&path).exists() {
-        println!("File not found");
-    }
 
     match wallpaper::set_from_path(&path.display().to_string()) {
         Ok(()) => println!("Success"),
