96e53232a32e35ae4e24d68cdb87f2c4caa909f7
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2024-02-24 02:19:51 +0500

    implemented config file

---
+++ src/config.rs
@@ -0,0 +1,84 @@
+use std::fs;
+use std::fs::File;
+use std::io::{Write};
+use std::io::prelude::*;
+use std::path::Path;
+use serde::{Deserialize, Serialize};
+use crate::error::Error;
+
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Config {
+    pub path: String,
+    pub interval: u32,
+}
+
+impl Config {
+    pub fn new<T>(path: T, interval: u32) -> Config
+    where
+        T: ToString,
+    {
+        Config { path: path.to_string(), interval }
+    }
+
+
+    pub fn from_file<T>(path: T) -> Result<Config, Error>
+    where
+        T: AsRef<Path>,
+    {
+        let file = File::open(&path);
+        
+        let mut file = match file {
+            Ok(file) => file,
+            Err(_err) => {
+                // let msg = format!("Error opening file {}: {}", path, err);
+                return Err(Error::FileOpenError);
+            }
+        };
+        
+        let mut content = String::new();
+
+        file.read_to_string(&mut content).expect("Couldn't read file to string");
+
+        let config = toml::from_str::<Config>(&content);
+
+        let result = match config {
+            Ok(d) => d,
+            Err(_) => return Err(Error::SerializeError)
+        };
+
+        Ok(result)
+    }
+
+    pub fn write<T>(&self, path: T) -> Result<(), Error>
+    where
+        T: ToString + Clone
+    {
+        let path = path.to_string();
+        let path = Path::new(&path);
+
+        if path.exists() {
+            std::fs::remove_file(path).expect("TODO: panic message");
+        }
+
+        let file = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(path);
+
+        let mut file = match file {
+            Ok(f) => f,
+            Err(_) => return Err(Error::WriteConfigError)
+        };
+
+        let deser = match toml::to_string_pretty(self) {
+            Ok(s) => s,
+            Err(_) => return Err(Error::DeserializeError)
+        };
+
+        file.write_all(deser.as_bytes()).expect("Failed writing to a file");
+
+        Ok(())
+    }
+}
